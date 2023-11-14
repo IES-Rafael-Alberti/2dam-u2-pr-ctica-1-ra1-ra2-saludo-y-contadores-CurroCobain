4519d54de54b250baeaea3a6f32e793a69154a2b
GaelCobain <fmunmar310@g.educaand.es>
2023-11-14 01:10:40 +0100

    faltan comentarios

--- app/src/main/java/com/fmunmar310/saludos_y_contadores/MainActivity.kt
+++ app/src/main/java/com/fmunmar310/saludos_y_contadores/MainActivity.kt
@@ -3,19 +3,23 @@ package com.fmunmar310.saludos_y_contadores
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,8 +27,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.fmunmar310.saludos_y_contadores.ui.theme.Saludos_y_ContadoresTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,12 +52,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Preview
-@Composable
-fun PreviewSaludoYcontadores(){
-    SaludoYcontadores()
-}
-@OptIn(ExperimentalMaterial3Api:: class)
 @Composable
 fun SaludoYcontadores(){
     var name by rememberSaveable { mutableStateOf("") }
@@ -56,10 +61,12 @@ fun SaludoYcontadores(){
     var cancel by rememberSaveable { mutableStateOf(0) }
     var showDialog by rememberSaveable { mutableStateOf(false) }
     if (showDialog){
-        MyAlertDialog(onDismissRequest = {showDialog = false} ,
-            onConfirmation = {accept++},
-            onCancel = {cancel++},
-            configuration ={name})
+        MyAlertDialog(
+            onDismissRequest = {showDialog = false} ,
+            onConfirmation = {accept++}, accValue = {accept.toString()},
+            onCancel = {cancel++}, canValue = {cancel.toString()},
+            onTextChange = {it : String -> name = it}, nameValue = {name}
+            )
 
     }
     Column(
@@ -70,10 +77,15 @@ fun SaludoYcontadores(){
     ) {
         Button(onClick = { showDialog = true
          }) {
-            Text(text = "Saludar")
+            Text(text = "Saludar",
+                fontSize = 30.sp)
         }
-        Text(text = "Saludos $name /$accept /$cancel ",
-            modifier = Modifier.wrapContentSize())
+        Spacer(modifier = Modifier.padding(30.dp))
+        Text(text = """
+            Saludos $name
+        """.trimIndent(),
+            modifier = Modifier.wrapContentSize(),
+            fontSize = 20.sp)
     }
 }
 @OptIn(ExperimentalMaterial3Api::class)
@@ -81,32 +93,68 @@ fun SaludoYcontadores(){
 fun MyAlertDialog(
     onDismissRequest: () -> Unit,
     onConfirmation: () -> Unit,
+    accValue: () -> String,
     onCancel: () -> Unit,
-    configuration: () -> Unit,
+    canValue: () -> String,
+    onTextChange: (String) -> Unit,
+    nameValue : () ->String
 
 ) {
-    var text by rememberSaveable { mutableStateOf("") }
-    AlertDialog(
+
+    Dialog(
         onDismissRequest = {
             onDismissRequest()
         },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onConfirmation()
+        properties = DialogProperties(dismissOnBackPress = true), {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize()
+                .background(color = Color.White)
+                .border(width = 1.dp, color = Color.Gray)
+                ){
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Row(){
+                        Spacer(modifier = Modifier.weight(1f))
+                        Text(text = "Configuración",
+                            modifier = Modifier.padding(bottom = 15.dp),
+                            fontStyle = FontStyle.Italic,
+                            fontSize = 25.sp
+                        )
+                    }
+                    Row(){
+                        OutlinedTextField(
+                            value = nameValue(),
+                            onValueChange = onTextChange,
+                            label = {Text("Ingresa tu nombre")},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 15.dp)
+                        )
+                    }
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 15.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly) {
+                        Button(onClick = {
+                            onConfirmation()
+                            onDismissRequest()})
+                        {
+                            Text("Conf ${accValue()}")}
+
+                        Button(onClick = {
+                            onCancel()
+                            onDismissRequest()
+                            onTextChange("")})
+                        {
+                            Text("Canc ${canValue()}")}
+
+                        Button(onClick = {
+                            onTextChange("")
+                        })
+                        {
+                            Text("Limp")}
+                    }
                 }
-            ) {
-                Text("Confirmar")
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    onDismissRequest()
-                    onCancel()
-                }
-            ) {
-                Text("Cancelar")
             }
         }
     )
